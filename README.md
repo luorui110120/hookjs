5fbe5c8b0eb19047f8222cbfda5bbc5744e979f3
luorui110120 <test9996889@gmali.com>
2020-10-21 14:45:50 +0800

    添加 hook 函数的一个参数, 这个参数的作用是”是否调用原始函数”

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ A hooks to any Javascript function.
 [bool]hook:params{
 	realFunc[String|must]:用于保存原始函数的函数名称,用于unHook;
 	hookFunc[Function|must]:替换的hook函数;
+	flagsCall[bool|opt]:是否掉用原来的函数,默认不调用;
 	context[Object|opt]:目标函数所在对象,用于hook非window对象下的函数，如String.protype.slice,carInstance1
 	methodName[String|opt]:匿名函数需显式传入目标函数名eg:this.Begin = function(){....};
 }
@@ -38,14 +39,14 @@ alert('Call' + name);
 var p = new Person();
 var _p_getName = null;
 function mygetName(name){alert("Hooked");}
-p.getName.hook("_p_getName",mygetName,p,"getName");
+p.getName.hook("_p_getName",mygetName,false,p,"getName");
 p.getName.unhook("_p_getName","getName",p);
 p.getName("pnig0s");
 
 //原型对象函数
 var _slice = null;
 function myslice(param){alert("Hooked");}
-String.prototype.slice.hook("_slice",myslice,String.prototype);
+String.prototype.slice.hook("_slice",myslice,false,String.prototype);
 String.prototype.slice.unhook("_slice","slice",String.prototype);
 var str = "pnig0s";
 str.slice(1);
--- hook.js
+++ hook.js
@@ -3,10 +3,10 @@ Code by pnig0s[Knownsec&FreeBuf]
 Date 20130214
 一個通用的js任意函數鉤子
 A simple hooks to Javascript functions
-
 [bool]hook:params{
 	realFunc[String|must]:用於保存原始的目標函數名,用於unHook;
 	hookFunc[Function|must]:替換的hook函數;
+	flagsCall[bool|opt]:是否掉用原来的函数,默认不调用;
 	context[Object|opt]:目標函數實例的對象,用於hook非window對象下的函數，如String.protype.slice,carInstance1
 	methodName[String|opt]:用於hook匿名函數eg:this.Begin = function(){....};
 }
@@ -15,19 +15,20 @@ A simple hooks to Javascript functions
 	funcName[String|must]:被Hook的函數名稱
 	context[Object|opt]:目標函數實例的對象,用於hook非window對象下的函數，如String.protype.slice,carInstance1
 }
-
 **/
 
 function Hooks()
 {
 	return {
 		initEnv:function () {
-			Function.prototype.hook = function (realFunc,hookFunc,context,funcName) {
+			Function.prototype.hook = function (realFunc,hookFunc,flagsCall,context,funcName) {
 				var _context = null; //函數上下文
 				var _funcName = null; //函數名
+				var _flagsCall = false; //判断是否原来的函数
 
 				_context = context || window;
 				_funcName = funcName || getFuncName(this);
+				_flagsCall = _flagsCall || flagsCall;
 				_context[realFunc] = this;
 
 				if(_context[_funcName].prototype && _context[_funcName].prototype.isHooked)
@@ -49,12 +50,20 @@ function Hooks()
 
 				try
 				{
-					eval('_context[_funcName] = function '+_funcName+'(){\n'+
-						'var args = Array.prototype.slice.call(arguments,0);\n'+
-						'var obj = this;\n'+
-						'hookFunc.apply(obj,args)\n'+
-						'return _context[realFunc].apply(obj,args);\n'+
-						'};');
+					var str='_context[_funcName] = function '+_funcName+'(){\n'+
+					'var args = Array.prototype.slice.call(arguments,0);\n'+
+					'var obj = this;\n';
+					if(_flagsCall)
+					{
+						str += 'hookFunc.apply(obj,args)\n'+
+						'return _context[realFunc].apply(obj,args);\n';
+					}
+					else
+					{
+						str += 'return hookFunc.apply(obj,args)\n';
+					}
+					str +='};';
+					eval(str);
 					_context[_funcName].prototype.isHooked = true;
 					return true;
 				}catch (e)
